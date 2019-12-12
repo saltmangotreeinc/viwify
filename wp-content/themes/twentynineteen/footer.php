88a73965cb04cb3177c6a5503cc604fc1e067063
Goutham Subramanyam <goutham@surveysparrow.com>
2019-12-12 21:26:17 +0530

    Landkit Index Added

--- wp-content/themes/twentynineteen/footer.php
+++ wp-content/themes/twentynineteen/footer.php
@@ -13,15 +13,215 @@
 
 ?>
 
-</div><!-- #content -->
+    <!-- FOOTER
+    ================================================== -->
+    <footer class="py-8 py-md-11 bg-gray-200">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-md-4 col-lg-3">
+        
+            <!-- Brand -->
+            <img src="<?php bloginfo('template_directory');?>/assets/img/brand.svg" alt="..." class="footer-brand img-fluid mb-2">
 
-	<footer class="zx-footer">
-		<h1>Footer</h1>
-	</footer><!-- #colophon -->
+            <!-- Text -->
+            <p class="text-gray-700 mb-2">
+              A better way to build.
+            </p>
 
-</div><!-- #page -->
+            <!-- Social -->
+            <ul class="list-unstyled list-inline list-social mb-6 mb-md-0">
+              <li class="list-inline-item list-social-item mr-3">
+                <a href="index.html#!" class="text-decoration-none">
+                  <img src="<?php bloginfo('template_directory');?>/assets/img/icons/social/instagram.svg" class="list-social-icon" alt="...">
+                </a>
+              </li>
+              <li class="list-inline-item list-social-item mr-3">
+                <a href="index.html#!" class="text-decoration-none">
+                  <img src="<?php bloginfo('template_directory');?>/assets/img/icons/social/facebook.svg" class="list-social-icon" alt="...">
+                </a>
+              </li>
+              <li class="list-inline-item list-social-item mr-3">
+                <a href="index.html#!" class="text-decoration-none">
+                  <img src="<?php bloginfo('template_directory');?>/assets/img/icons/social/twitter.svg" class="list-social-icon" alt="...">
+                </a>
+              </li>
+              <li class="list-inline-item list-social-item">
+                <a href="index.html#!" class="text-decoration-none">
+                  <img src="<?php bloginfo('template_directory');?>/assets/img/icons/social/pinterest.svg" class="list-social-icon" alt="...">
+                </a>
+              </li>
+            </ul>
 
-<?php wp_footer(); ?>
+          </div>
+          <div class="col-6 col-md-4 col-lg-2">
+        
+            <!-- Heading -->
+            <h6 class="font-weight-bold text-uppercase text-gray-700">
+              Products
+            </h6>
 
-</body>
+            <!-- List -->
+            <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Page Builder
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  UI Kit
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Styleguide
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a href="index.html#!" class="text-reset">
+                  Changelog
+                </a>
+              </li>
+            </ul>
+
+          </div>
+          <div class="col-6 col-md-4 col-lg-2">
+        
+            <!-- Heading -->
+            <h6 class="font-weight-bold text-uppercase text-gray-700">
+              Services
+            </h6>
+
+            <!-- List -->
+            <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Documentation
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Changelog
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Pagebuilder
+                </a>
+              </li>
+              <li>
+                <a href="index.html#!" class="text-reset">
+                  UI Kit
+                </a>
+              </li>
+            </ul>
+
+          </div>
+          <div class="col-6 col-md-4 offset-md-4 col-lg-2 offset-lg-0">
+        
+            <!-- Heading -->
+            <h6 class="font-weight-bold text-uppercase text-gray-700">
+              Connect
+            </h6>
+
+            <!-- List -->
+            <ul class="list-unstyled text-muted mb-0">
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Page Builder
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  UI Kit
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Styleguide
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Documentation
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Changelog
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Documentation
+                </a>
+              </li>
+              <li>
+                <a href="index.html#!" class="text-reset">
+                  Changelog
+                </a>
+              </li>
+            </ul>
+
+          </div>
+          <div class="col-6 col-md-4 col-lg-2">
+        
+            <!-- Heading -->
+            <h6 class="font-weight-bold text-uppercase text-gray-700">
+              Legal
+            </h6>
+
+            <!-- List -->
+            <ul class="list-unstyled text-muted mb-0">
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Documentation
+                </a>
+              </li>
+              <li class="mb-3">
+                <a href="index.html#!" class="text-reset">
+                  Changelog
+                </a>
+              </li>
+              <li>
+                <a href="index.html#!" class="text-reset">
+                  Pagebuilder
+                </a>
+              </li>
+            </ul>
+
+          </div>
+        </div> <!-- / .row -->
+      </div> <!-- / .container -->
+    </footer>
+
+    <!-- JAVASCRIPT
+    ================================================== -->
+    <!-- Libs JS -->
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/jquery/dist/jquery.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/flickity/dist/flickity.pkgd.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/flickity-fade/flickity-fade.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/aos/dist/aos.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/smooth-scroll/dist/smooth-scroll.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/jarallax/dist/jarallax.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/jarallax/dist/jarallax-video.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/jarallax/dist/jarallax-element.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/typed.js/lib/typed.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/countup.js/dist/countUp.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/highlightjs/highlight.pack.min.js"></script>
+    <script src="<?php bloginfo('template_directory');?>/assets/libs/@fancyapps/fancybox/dist/jquery.fancybox.min.js"></script>
+
+    <!-- Map -->
+    <script src='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.js'></script>
+
+    <!-- Theme JS -->
+    <script src="<?php bloginfo('template_directory');?>/assets/js/theme.min.js"></script>
+		<?php wp_footer(); ?>
+  </body>
 </html>
--- wp-content/themes/twentynineteen/header.php
+++ wp-content/themes/twentynineteen/header.php
@@ -10,17 +10,30 @@
  * @subpackage Twenty_Nineteen
  * @since 1.0.0
  */
-?><!doctype html>
+?>
+<!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="profile" href="https://gmpg.org/xfn/11" />
-	<?php wp_head(); ?>
-</head>
+  <head>
+    <!-- Required meta tags -->
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="profile" href="https://gmpg.org/xfn/11" />
+		<?php wp_head(); ?>
+		<!-- Libs CSS -->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/fonts/Feather/feather.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/libs/flickity/dist/flickity.min.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/libs/flickity-fade/flickity-fade.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/libs/aos/dist/aos.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/libs/jarallax/dist/jarallax.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/libs/highlightjs/styles/vs2015.css">
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/libs/@fancyapps/fancybox/dist/jquery.fancybox.min.css">
 
-<body>
-<?php wp_body_open(); ?>
-<header class="zx-header">
-	<h1>Header</h1>
-</header>
+    <!-- Map -->
+    <link href='https://api.mapbox.com/mapbox-gl-js/v0.53.0/mapbox-gl.css' rel='stylesheet' />
+
+    <!-- Theme CSS -->
+    <link rel="stylesheet" href="<?php bloginfo('template_directory');?>/assets/css/theme.min.css">
+
+    <title>Landkit</title>
+  </head>
+  <body>

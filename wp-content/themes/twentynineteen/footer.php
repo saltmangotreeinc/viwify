7767fd8c139adfee6b6105d4451026f5ba56e187
Goutham Subramanyam <goutham@surveysparrow.com>
2019-12-14 18:22:43 +0530

    Footer Done

--- wp-content/themes/twentynineteen/footer.php
+++ wp-content/themes/twentynineteen/footer.php
@@ -18,7 +18,7 @@
     <footer class="py-8 py-md-11 bg-gray-200">
       <div class="container">
         <div class="row">
-          <div class="col-12 col-md-4 col-lg-3">
+          <div class="col-12 col-md-12 col-lg-3 text-center pb-md-8 text-lg-left">
         
             <!-- Brand -->
             <img src="<?php bloginfo('template_directory');?>/assets/img/brand.svg" alt="..." class="footer-brand img-fluid mb-2">
@@ -53,7 +53,7 @@
             </ul>
 
           </div>
-          <div class="col-6 col-md-4 col-lg-2">
+          <div class="col-12 col-md-4 col-lg-3 offset-lg-2 text-center text-lg-left">
         
             <!-- Heading -->
             <h6 class="font-weight-bold text-uppercase text-gray-700">
@@ -63,134 +63,52 @@
             <!-- List -->
             <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
               <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Page Builder
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  UI Kit
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Styleguide
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Documentation
-                </a>
-              </li>
-              <li>
-                <a href="index.html#!" class="text-reset">
-                  Changelog
+                <a href="/products" class="text-reset">
+                  Interactive Displays
                 </a>
               </li>
             </ul>
 
           </div>
-          <div class="col-6 col-md-4 col-lg-2">
+          <div class="col-12 col-md-4 col-lg-2 text-center text-lg-left">
         
             <!-- Heading -->
             <h6 class="font-weight-bold text-uppercase text-gray-700">
-              Services
+              Industries
             </h6>
 
             <!-- List -->
             <ul class="list-unstyled text-muted mb-6 mb-md-8 mb-lg-0">
               <li class="mb-3">
                 <a href="index.html#!" class="text-reset">
-                  Documentation
+                  Education
                 </a>
               </li>
               <li class="mb-3">
                 <a href="index.html#!" class="text-reset">
-                  Changelog
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Pagebuilder
-                </a>
-              </li>
-              <li>
-                <a href="index.html#!" class="text-reset">
-                  UI Kit
+                  Business
                 </a>
               </li>
             </ul>
 
           </div>
-          <div class="col-6 col-md-4 offset-md-4 col-lg-2 offset-lg-0">
+          <div class="col-12 col-md-4 col-lg-2 offset-lg-0 text-center text-lg-left">
         
             <!-- Heading -->
             <h6 class="font-weight-bold text-uppercase text-gray-700">
-              Connect
+              Company
             </h6>
 
             <!-- List -->
             <ul class="list-unstyled text-muted mb-0">
               <li class="mb-3">
                 <a href="index.html#!" class="text-reset">
-                  Page Builder
+                  About Us
                 </a>
               </li>
               <li class="mb-3">
                 <a href="index.html#!" class="text-reset">
-                  UI Kit
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Styleguide
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Documentation
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Changelog
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Documentation
-                </a>
-              </li>
-              <li>
-                <a href="index.html#!" class="text-reset">
-                  Changelog
-                </a>
-              </li>
-            </ul>
-
-          </div>
-          <div class="col-6 col-md-4 col-lg-2">
-        
-            <!-- Heading -->
-            <h6 class="font-weight-bold text-uppercase text-gray-700">
-              Legal
-            </h6>
-
-            <!-- List -->
-            <ul class="list-unstyled text-muted mb-0">
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Documentation
-                </a>
-              </li>
-              <li class="mb-3">
-                <a href="index.html#!" class="text-reset">
-                  Changelog
-                </a>
-              </li>
-              <li>
-                <a href="index.html#!" class="text-reset">
-                  Pagebuilder
+                  Out Mission
                 </a>
               </li>
             </ul>
--- wp-content/themes/twentynineteen/header.php
+++ wp-content/themes/twentynineteen/header.php
@@ -70,8 +70,8 @@
         <!-- Navigation -->
         <ul class="navbar-nav ml-auto">
           <li class="nav-item dropdown">
-            <a class="nav-link" href="/product">
-              Product
+            <a class="nav-link" href="/products">
+              Products
             </a>
           </li>
           <li class="nav-item dropdown">

24ec9ff645a44c386e25bccede75f96d13687c98
Edwin Babu <edwin@surveysparrow.com>
2020-03-15 17:20:59 +0530

    contact-form and tables created

--- wp-content/themes/twentynineteen/about-us.php
+++ wp-content/themes/twentynineteen/about-us.php
@@ -189,5 +189,136 @@ Template Name: About Us
     </div> <!-- / .row -->
   </div> <!-- / .container -->
 </section>
+<section class="py-10 py-md-11">
+<div class="container">
+        <form name="frmContact" id="" frmContact"" method="post"
+            action="" enctype="multipart/form-data"
+            onsubmit="return validateContactForm()">
+
+            <div class="input-row">
+                <label style="padding-top: 20px;">Name</label> <span
+                    id="userName-info" class="info"></span><br /> <input
+                    type="text" class="input-field" name="userName"
+                    id="userName" />
+            </div>
+            <div class="input-row">
+                <label>Email</label> <span id="userEmail-info"
+                    class="info"></span><br /> <input type="text"
+                    class="input-field" name="userEmail" id="userEmail" />
+            </div>
+            <div class="input-row">
+                <label>Subject</label> <span id="subject-info"
+                    class="info"></span><br /> <input type="text"
+                    class="input-field" name="subject" id="subject" />
+            </div>
+            <div class="input-row">
+                <label>Message</label> <span id="userMessage-info"
+                    class="info"></span><br />
+                <textarea name="content" id="content"
+                    class="input-field" cols="60" rows="6"></textarea>
+            </div>
+            <div>
+                <input type="submit" name="send" class="btn-submit"
+                    value="Send" />
+
+                <div id="statusMessage"> 
+                        <?php
+                        if (! empty($message)) {
+                            ?>
+                            <p class='<?php echo $type; ?>Message'><?php echo $message; ?></p>
+                        <?php
+                        }
+                        ?>
+                    </div>
+            </div>
+        </form>
+    </div>
+</section>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"
+        type="text/javascript"></script>
+    <script type="text/javascript">
+        function validateContactForm() {
+            var valid = true;
+
+            $(".info").html("");
+            $(".input-field").css('border', '#e0dfdf 1px solid');
+            var userName = $("#userName").val();
+            var userEmail = $("#userEmail").val();
+            var subject = $("#subject").val();
+            var content = $("#content").val();
+            
+            if (userName == "") {
+                $("#userName-info").html("Required.");
+                $("#userName").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (userEmail == "") {
+                $("#userEmail-info").html("Required.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (!userEmail.match(/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/))
+            {
+                $("#userEmail-info").html("Invalid Email Address.");
+                $("#userEmail").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+
+            if (subject == "") {
+                $("#subject-info").html("Required.");
+                $("#subject").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            if (content == "") {
+                $("#userMessage-info").html("Required.");
+                $("#content").css('border', '#e66262 1px solid');
+                valid = false;
+            }
+            return valid;
+        }
+</script>
+
+<?php
+
+if(!empty($_POST["send"])) {
+	$name = $_POST["userName"];
+	$email = $_POST["userEmail"];
+	$subject = $_POST["subject"];
+  $content = $_POST["content"];
+  insertIntoContact($name,$email,$subject,$content);
+}
+
+function insertIntoContact($name,$email,$subject,$content){
+  global $wpdb;
+  $prefix ='wp_';
+  $table = $wpdb->$prefix.'wp_contact';
+  $data = array('name' => $name, 'email' => $email, 'subject'=>$subject, 'content'=>$content);
+  $format = array('%s','%s');
+  echo "format---<=>",$email;
+  $wpdb->insert($table,$data,$format);
+  $my_id = $wpdb->insert_id;
+  if($my_id ==0){
+    echo "Error:".$wpdb->last_error;
+  } else {
+    echo "Inserted into table".$my_id;
+  }
+}
+
+function show_all_tables(){
+  global $wpdb;
+  foreach($wpdb->get_results("SHOW TABLES", ARRAY_N) as $table): 
+      echo $table[0]."<br/>"; 
+  endforeach;
+} 
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+?>
+
 
 <?php get_footer(); ?>

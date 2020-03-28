7c3765b2774b75ca85c7496cd6af6889d533242c
Edwin Babu <edwinbbu@gmail.com>
2020-03-28 23:04:02 +0530

    demo emails inserted

--- wp-content/themes/twentynineteen/template-parts/components/book-demo.php
+++ wp-content/themes/twentynineteen/template-parts/components/book-demo.php
@@ -1,7 +1,33 @@
 <div class="vw-book-demo--wrapper">
-  <form class="vw-book-demo d-flex">
-    <input type="email" name="email" placeholder="Email Address" required/>
+  <form class="vw-book-demo d-flex" method="POST" onsubmit="return submitEmail()">
+    <input type="email" name="email" id="demo-email" placeholder="Email Address" required/>
     <button type="submit" data-loading-text="Booking...">Book a Demo</button>
   </form>
   <p class="vw--14 vw--light">A personalized consultation with our product expert</p>
 </div>
+
+<script type="text/javascript">
+    function submitEmail(){
+      var email=document.getElementById('demo-email').value;
+      $.ajax({
+        type:"post",
+        url:"<?php bloginfo('template_directory');?>/template-parts/components/submit-email.php",
+        data: 
+        {  
+          'email' :email,
+        },
+        cache:false,
+        success: function (html) 
+        {
+          console.log('Inserted into db:', $.parseJSON(html));
+          $('#msg').html(html);
+        },
+        error : function(err) 
+        {
+          console.log('Error:',err);
+        }
+        });
+      document.getElementById('demo-email').value = '';
+      return false;
+     }
+</script>
---
+++ wp-content/themes/twentynineteen/template-parts/components/submit-email.php
@@ -0,0 +1,22 @@
+<?php 
+require_once( $_SERVER['DOCUMENT_ROOT'].'/wp-load.php' );
+$email = $_POST['email'];
+
+if(!empty($email)) {
+  try{
+    $prefix ='wp_';
+    $table = $wpdb->$prefix.'demo';
+    $data = array('email' => $email);
+    $format = array('%s','%s');
+    $wpdb->insert($table,$data,$format);
+    $my_id = $wpdb->insert_id;
+    if($my_id ==0){
+      echo json_encode($wpdb->last_error);
+    } else {
+      echo json_encode($my_id);
+    }
+  } catch(Exception $e){
+    echo $e;
+  }
+}
+?>
